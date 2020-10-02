edc485593f6112b3156f94c77d2fd08988211d60
adenild <adenildojunior52@gmail.com>
2020-10-02 15:21:33 -0300

    Added funtions

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import os
 
 
 def main():
-    file_name = input("Qual vai ser o nome do arquivo? ")
+    file_name = 'labels.txt'  # input("Qual vai ser o nome do arquivo? ")
 
-    # Define a janela do Tkinter
     window = Tk()
     window.title('Não é o Labelme')
 
-    # Pega a lista das imagens
     images = []
     for file in os.listdir('images/'):
         if '.txt' in file:
@@ -20,26 +18,35 @@ def main():
     lines = open(f"outputs/{file_name}", 'r').readlines()
     index = 0
     if len(lines) >= 1:
-        print(lines[-1])
-        index = lines.index(lines[-1])+1
-        print(index)
+        last_line = lines.index(lines[-1])
+        index = last_line
+        last_line_string = open(f"outputs/{file_name}", "r").readlines()[last_line]
+        line = index-1
     else:
         pass
 
     total_images = len(images)
     print(f"Existem {total_images} imagens.")
+    print(f"Você está atualmente na imagem {images[index]}")
 
-    # Salva a imagem no Tk
     im = Image.open(f"images/{images[index]}")
     im.thumbnail((800, 600))
     ph = ImageTk.PhotoImage(im)
 
-    # Exibe a imagem no Tk
     label = Label(image=ph)
     label.image = ph
     label.grid(row=0, column=0, columnspan=3)
 
-    # Muda pra próxima imagem e salva o texto
+    def save(output: str):
+        open(f'outputs/{file_name}', 'a+').write(output)
+        print(f"Você digitou {output.split(',')[2]} no arquivo {images[index]}")
+
+    def edit(output: str):
+        lines_list = open(f"outputs/{file_name}", "r").readlines()
+        if lines_list[line] != output:
+            lines_list[line] = output
+            open(f"outputs/{file_name}", "w").writelines(lines_list)
+
     # TODO: Consertar o final do loop. Ta travando o programa
     # TODO: Fazer a validação dos dados
     def next_image():
@@ -48,35 +55,85 @@ def main():
         nonlocal im
         nonlocal ph
 
-        # Informa a posição atual
-        print(f"[{index+1}/{total_images}]")
+        output = f"{images[index]},None,{input_box.get()}\n"
+        if last_line_string.replace('\n', '') == output.replace('\n', '') or input_box.get() == '':
+            input_box.delete(0, 'end')
 
-        # Salva no arquivo o texto digitado
-        label_file = open(f'outputs/{file_name}', 'a+')
-        label_file.write(f"{images[index]},None,{input_box.get()}\n")
-        input_box.delete(0, 'end')
-        print(f"Você digitou {input_box.get()} no arquivo {images[index]}")
-        label_file.close()
+            index += 1
 
-        # Avança na lista
-        index += 1
+            label.grid_forget()
+            im = Image.open(f"images/{images[index]}")
+            im.thumbnail((800, 600))
+            ph = ImageTk.PhotoImage(im)
+            label = Label(image=ph)
+            label.image = ph
+            label.grid(row=0, column=0, columnspan=3)
+        else:
+            print(f"[{index+1}/{total_images}] - {images[index+1]}")
 
-        # Reseta a janela e coloca a imagem nova
-        label.grid_forget()
-        im = Image.open(f"images/{images[index]}")
-        im.thumbnail((800, 600))
-        ph = ImageTk.PhotoImage(im)
-        label = Label(image=ph)
-        label.image = ph
-        label.grid(row=0, column=0, columnspan=3)
+            save(output)
+            input_box.delete(0, 'end')
 
-    # Declaração da caixa de texto
+            index += 1
+
+            label.grid_forget()
+            im = Image.open(f"images/{images[index]}")
+            im.thumbnail((800, 600))
+            ph = ImageTk.PhotoImage(im)
+            label = Label(image=ph)
+            label.image = ph
+            label.grid(row=0, column=0, columnspan=3)
+
+    def previous_image():
+        nonlocal index
+        nonlocal label
+        nonlocal im
+        nonlocal ph
+
+        output = f"{images[index]},None,{input_box.get()}\n"
+        if input_box.get() == '':
+            if index > 0:
+                index -= 1
+
+                input_box.delete(0, 'end')
+                input_box.insert(0, lines[index].split(',')[2].replace('\n', ''))
+
+                label.grid_forget()
+                im = Image.open(f"images/{images[index]}")
+                im.thumbnail((800, 600))
+                ph = ImageTk.PhotoImage(im)
+                label = Label(image=ph)
+                label.image = ph
+                label.grid(row=0, column=0, columnspan=3)
+            else:
+                pass
+        else:
+            if index > 0:
+                edit(output)
+                index -= 1
+
+                input_box.delete(0, 'end')
+                input_box.insert(0, lines[index].split(',')[2].replace('\n', ''))
+
+                label.grid_forget()
+                im = Image.open(f"images/{images[index]}")
+                im.thumbnail((800, 600))
+                ph = ImageTk.PhotoImage(im)
+                label = Label(image=ph)
+                label.image = ph
+                label.grid(row=0, column=0, columnspan=3)
+
+    button_previous = Button(window, text="Anterior", command=previous_image)
+    button_previous.grid(row=1, column=0)
+
     input_box = Entry(window, width=100)
-    input_box.grid(row=1, column=0)
+    input_box.grid(row=1, column=1)
 
-    # Declaração do botão
     button_next = Button(window, text="Próximo", command=next_image)
-    button_next.grid(row=1, column=1)
+    button_next.grid(row=1, column=2)
+
+    if index > 0:
+        input_box.insert(0, lines[index].split(',')[2].replace('\n', ''))
 
     window.mainloop()
 
