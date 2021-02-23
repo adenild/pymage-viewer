ea6e368eeef63a53ba9d100a5bb28b9cc4c4b656
Adenildo Junior <adenildojunior52@gmail.com>
2021-02-23 00:31:40 -0300

    :wastebasket: remove: Removidos os antigos arquivos do projeto

--- dependencies/side.py
+++ dependencies/side.py
@@ -6,19 +6,36 @@ import json
 
 
 def select_output_file(debug=False):
+    """
+    Pergunta o nome do arquivo em que os dados serão salvos no console, caso não exista ele cria um.
+
+    :param debug: Booleano que ativa o modo de testes
+    :return: Caminho do arquivo em que os dados serão salvos
+    """
     if debug:
         return "debug/labels.json"
     return input("Digite o nome do arquivo: ")
 
 
 def select_images_dir(debug=False):
+    """
+    Abre um diálogo para a seleção de uma pasta, onde ficam as imagens que receberão label.
+
+    :param debug: Booleano que ativa o modo de testes
+    :return: O caminho da pasta que contém as imagens
+    """
     if debug:
-        return "images/placa"
+        return "debug/placa"
     return filedialog.askdirectory()
 
 
 def image_loader(images_dir):
-    # Pega a lista das imagens
+    """
+    Gera uma lista com os nomes das imagens, que será utilizada junto com a pasta delas pra exibição.
+
+    :param images_dir: Caminho das imagens
+    :return: Lista com cada objeto sendo um nome de imagem
+    """
     images = []
     image_extensions = ['png', 'jpg', 'jpeg', 'tif', 'gif']
     for file in os.listdir(f'{images_dir}/'):
@@ -32,7 +49,13 @@ def image_loader(images_dir):
     return images
 
 
-def load_data(file, images_list=None):
+def load_data(file):
+    """
+    Carrega o arquivo JSON com os dados, transformando tudo em um objeto Python.
+
+    :param file: Caminho pro objeto JSON que será lido
+    :return: Objeto Python com os mesmo dados contidos no JSON
+    """
     print("Carreguei")
     try:
         with open(f"{file}", "r") as read_file:
@@ -44,19 +67,33 @@ def load_data(file, images_list=None):
 
 
 def save_data_json(file, data):
+    """
+    Salva o objeto Python novamente no arquivo JSON.
+
+    :param file: Arquivo em que serão gravados os dados
+    :param data: Dados a serem gravados no arquivo
+    """
     with open(f"{file}", "w") as write_file:
         json.dump(data, write_file, indent=1)
 
 
-# TODO: Encontrar um novo nome pra função
-def starter_list(data):
-    labeled_files = []
-    for key in data.keys():
-        if len(data[key]["label"]) > 0:
-            labeled_files.append(key)
+def format_data(data):
+    """
+    Formata os dados, retornando um dicionário com 3 chaves. Essas chaves contém cada lista de imagem
 
-    print(f'{len(labeled_files)} imagens ja foram detectadas no arquivo de entrada.')
-    return labeled_files
+    :param data: A data crua, recém carregada do arquivo
+    :return:
+    """
+    files_dict = {"all_files": {}, "labelled_files": {}, "not_labelled_files": {}}
+    for key in data.keys():
+        files_dict["all_files"][key] = (data[key])
+        if len(data[key]["label"]) > 0:
+            files_dict["labelled_files"][key] = (data[key])
+        else:
+            files_dict["not_labelled_files"][key] = (data[key])
+
+    print(f'{len(files_dict["labelled_files"])} imagens foram detectadas no arquivo de entrada.')
+    return files_dict
 
 
 def setup_image(images_dir, list_position):
--- main.py
+++ main.py
@@ -1,44 +1,79 @@
 from tkinter import *
 import tkinter.font as font
-from dependencies import commands, widgets, side
+from dependencies import commands, side
 
 
 def main():
+    # Configuração do aplicativo
     window = Tk()
     window.title('Simple Label')
     system_font = font.Font(size=16)
-    file = side.select_output_file()
-    images_dir = side.select_images_dir()
+    file = side.select_output_file(True)
+    images_dir = side.select_images_dir(True)
 
+    # Requisição da data
     images = side.image_loader(images_dir)
-    data = side.load_data(file, images)
+    data = side.load_data(file)
+    files_dict = side.format_data(data)
+    """for item in files_dict:
+        print(item)
 
-    labeled_files = side.starter_list(data)  # TODO: Dar um uso para os arquivos que já receberam label
-    total_images = len(images)
+    # Seleção da lista de imagens
+    print("\033[96mQual a lista de imagem que você deseja ver?")
+    images_list = input("\033[95m 1. Todas as imagens\n\033[94m 2. Imagens com label\n\033[92m 3. Imagens sem "
+                        "label\n\033[96mR: ")
+    if images_list == "1":
+        data = files_dict["all_files"]
+    elif images_list == "2":
+        data = files_dict["labelled_files"]
+        images = [image for image in files_dict["labelled_files"].keys()]
+    elif images_list == "3":
+        data = files_dict["not_labelled_files"]
+        images = [image for image in files_dict["not_labelled_files"].keys()]"""
+
+    # Cálculo de métricas
+    total_images, total_dataset = len(images), len(data)
     index = 0
-
     if total_images == 0:
         print('Todas as imagens da pasta já receberam labels')
     else:
-        print(f"Existem {total_images} imagens.")
+        print(f"Existem {total_images} imagens. Você está visualizando uma lista com {total_dataset} imagens.")
 
+    # Renderização da imagem
     image = side.setup_image(images_dir, images[index])
     label = Label(image=image)
-    label.image = image
     label.grid(row=0, column=0, columnspan=3)
 
+    # Renderização do input
     input_box = Entry(window, width=60)
     input_box["font"] = system_font
     input_box.grid(row=1, column=0)
     input_box.insert(0, side.check_labeled(images[index], data))
 
-    local_vars = [index, label, image, total_images, input_box, images, data, file, images_dir, window]
+    local_vars = [index, label, image, total_images, input_box, images, data, file, images_dir,
+                  window]  # TODO: Remover isso urgente
+
+    # Renderização dos botões de Ir e Voltar
     button_prev = Button(window, text="Anterior", command=lambda: commands.change_image("prev", local_vars))
     button_prev.grid(row=1, column=2)
     button_next = Button(window, text="Próximo", command=lambda: commands.change_image("next", local_vars))
     button_next.grid(row=1, column=3)
 
-    widgets.generate_menubar(window, local_vars)
+    # Renderização da barra de menus
+    menu_bar = Menu(window)
+    file_menu = Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label="Open", command=lambda file=file: side.load_data(file))
+    file_menu.add_command(label="Save", command=lambda file=file, data=data: side.save_data_json(file, data))
+    file_menu.add_separator()
+    file_menu.add_command(label="Exit", command=window.quit)
+    menu_bar.add_cascade(label="File", menu=file_menu)
+    label_menu = Menu(menu_bar, tearoff=0)
+    label_menu.add_command(label="All Images", command=lambda: commands.toggle_data_list(local_vars, 'all'))
+    label_menu.add_command(label="Labelled", command=lambda: commands.toggle_data_list(local_vars, 'labelled'))
+    label_menu.add_command(label="Not Labelled", command=lambda: commands.toggle_data_list(local_vars, 'not labelled'))
+    menu_bar.add_cascade(label="Edit", menu=label_menu)
+    window.config(menu=menu_bar)
+
     window.mainloop()
 
 
